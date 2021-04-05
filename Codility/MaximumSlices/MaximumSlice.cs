2aa485063820458d8475590b142f1cd2dd4eb8f7
Milan Assuied <milan.assuied@opal-rt.com>
2021-04-05 12:30:39 -0400

    MaximumProfit

---
+++ Codility/MaximumSlices/MaximumSlice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Codility.MaximumSlices
+{
+    public class MaximumSlice
+    {
+        public int solution(int[] A)
+        {
+            int globalMaxSum = 0;
+            int localMaxSum = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                localMaxSum = Math.Max(A[i], localMaxSum + A[i]);
+                globalMaxSum = Math.Max(localMaxSum, globalMaxSum);
+            }
+
+            return globalMaxSum;
+        }
+    }
+}
